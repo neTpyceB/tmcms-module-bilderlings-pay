59726f5f32a207810ca1d6f92601728b6002d4df
Vadims Petrusevs <pvman@inbox.ru>
2018-05-14 21:34:38 +0200

    Module files inited

---
+++ src/CmsBilderlingsPay.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace TMCms\Modules\BilderlingsPay;
+
+\defined('INC') or exit;
+
+/**
+ * Class CmsBilderlingsPay
+ * @package TMCms\Modules\BilderlingsPay
+ */
+class CmsBilderlingsPay
+{
+    public static function _default()
+    {
+        echo 'TODO';
+    }
+}
---
+++ src/ModuleBilderlingsPay.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace TMCms\Modules\BilderlingsPay;
+
+use TMCms\Modules\IModule;
+use TMCms\Traits\singletonInstanceTrait;
+
+\defined('INC') or exit;
+
+/**
+ * Class ModuleBilderlingsPay
+ * @package TMCms\Modules\BilderlingsPay
+ */
+class ModuleBilderlingsPay implements IModule
+{
+    use singletonInstanceTrait;
+}
